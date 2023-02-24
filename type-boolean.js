bb5e2b4ea35a43a4b7a4cf106677ca052d3c7ab9
Rosana <111008983+Rosanasantana84@users.noreply.github.com>
2023-02-23 21:56:24 -0300

    type-variáveis
    let - if - else

--- type-boolean.js
+++ type-boolean.js
@@ -1,6 +1,14 @@
 const primeiroNumero = 5;
 const segundoNumero = 5;
+const idade = 2;
+const nome = "Lara";
+const sobrenome = "Ayumi";
+const bacharel = false;
+const dezAnosDepois = "12";
 
-console.log(primeiroNumero === segundoNumero);
+
+
+console.log = nome + dezAnosDepois
+
 
 
