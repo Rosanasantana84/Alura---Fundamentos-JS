8587d788a1d8935d5c9b87a0f267ad66247a39e4
Rosana <111008983+Rosanasantana84@users.noreply.github.com>
2023-02-25 19:51:06 -0300

    Tipos de variáveis
    truly/false/let

---
+++ type-conversoes.js
@@ -0,0 +1,14 @@
+//converções
+
+// boolean
+// conersão implícita
+
+const numero = 456;
+const numeroString = Number("456");
+
+console.log(numero + numeroString)
+
+
+// conversão explícita
+
+
---
+++ type-truthyefalse.js
@@ -0,0 +1,25 @@
+//boolean
+
+const usuárioLogado = true;
+const contaPaga = false;
+
+// truthy ou falsy
+
+// 0 = > false
+// 1 => true
+
+//console.log(0 == false)
+//console.log("" == false)
+//console.log(1 == false)
+
+//null
+// undefined
+
+let minhaVar;
+let varNull = null
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof  minhaVar)
+console.log(typeof varNull)
